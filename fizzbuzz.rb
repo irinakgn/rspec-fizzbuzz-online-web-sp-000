cc7703af5973d7ca6dafb0994b1353adc473e0a8
mirthful-flag-5763 <irinagamb@gmail.com>
2019-03-21 02:21:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,11 @@
 def fizzbuzz(int)
   if int % 3 == 0  &&  int % 5 == 0 # if the number int is divisible by 3
     "FizzBuzz" # Go fizz
-  
-elseif int % 3 == 0 # if the number int is divisible by 3
+
+elsif int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  end
-  if else int % 5 == 0 # if the number int is divisible by 3
+
+  elsif int % 5 == 0 # if the number int is divisible by 3
     "Buzz" # Go fizz
   end
 end
